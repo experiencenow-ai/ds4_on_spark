f6cf67653144c07c4c43c0cc45cea4d0e44d39df
ethpred <144991674+ethpred@users.noreply.github.com>
2026-05-08 23:01:34 +0000

    build-skeleton: make CUDA status/test portable

--- tests/test_cuda.c
+++ tests/test_cuda.c
@@ -4,14 +4,23 @@
 
 int32_t test_cuda(void)
 {
-	ds4_cuda_status_t st;
+	ds4_cuda_status_t st0,st1,st2;
 	const char *s;
-	st = ds4_cuda_last_error();
-	if ( ds4_cuda_is_ok(st) != 0 )
+	st0 = ds4_cuda_ok();
+	if ( ds4_cuda_is_ok(st0) == 0 )
 		return(-1);
-	s = ds4_cuda_errstr(st);
-	if ( s == 0 )
+	st1 = ds4_cuda_fail(123);
+	if ( ds4_cuda_is_ok(st1) != 0 )
 		return(-2);
+	s = ds4_cuda_errstr(st0);
+	if ( s == 0 )
+		return(-3);
+	s = ds4_cuda_errstr(st1);
+	if ( s == 0 )
+		return(-4);
+	st2 = ds4_cuda_last_error();
+	s = ds4_cuda_errstr(st2);
+	if ( s == 0 )
+		return(-5);
 	return(0);
 }
-
