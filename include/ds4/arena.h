385747f09e2e947ddb600c36f43d7155f8cde899
experiencenow-ai <opustrace@gmail.com>
2026-05-09 21:57:22 +0900

    build-skeleton: C++ consumer header safety (#142)
    Co-authored-by: ethpred <144991674+ethpred@users.noreply.github.com>

--- include/ds4/arena.h
+++ include/ds4/arena.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <stdint.h>
+#include "ds4/common.h"
 
 typedef struct
 {
@@ -8,8 +8,10 @@ typedef struct
 	int32_t size,used;
 } ds4_arena_t;
 
+DS4_EXTERN_C_BEGIN
 int32_t ds4_arena_init(ds4_arena_t *a,uint8_t *mem,int32_t size);
 int32_t ds4_arena_reset(ds4_arena_t *a);
 int32_t ds4_arena_mark(ds4_arena_t *a,int32_t *out_mark);
 int32_t ds4_arena_release(ds4_arena_t *a,int32_t mark);
 int32_t ds4_arena_alloc(ds4_arena_t *a,int32_t size,int32_t align,void **out);
+DS4_EXTERN_C_END
--- include/ds4/log.h
+++ include/ds4/log.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <stdint.h>
+#include "ds4/common.h"
 
 typedef enum
 {
@@ -12,10 +12,12 @@ typedef enum
 
 typedef void (*ds4_log_sink_fn)(void *ctx,int32_t level,const char *msg);
 
+DS4_EXTERN_C_BEGIN
 const char *ds4_log_level_name(int32_t level);
 int32_t ds4_log_set_level(int32_t level);
 int32_t ds4_log_set_sink(ds4_log_sink_fn fn,void *ctx);
 int32_t ds4_logf(int32_t level,const char *fmt,...);
+DS4_EXTERN_C_END
 
 #define DS4_LOGE(...) ds4_logf(DS4_LOG_ERROR,__VA_ARGS__)
 #define DS4_LOGW(...) ds4_logf(DS4_LOG_WARN,__VA_ARGS__)
@@ -28,5 +30,7 @@ typedef struct
 	int32_t cap,used,truncated;
 } ds4_log_buf_t;
 
+DS4_EXTERN_C_BEGIN
 int32_t ds4_log_buf_init(ds4_log_buf_t *lb,char *buf,int32_t cap);
 void ds4_log_buf_sink(void *ctx,int32_t level,const char *msg);
+DS4_EXTERN_C_END
