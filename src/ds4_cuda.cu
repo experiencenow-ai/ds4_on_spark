8213b4673c9e65f1d246b4d8d6a60085e0ff3730
ethpred <144991674+ethpred@users.noreply.github.com>
2026-05-09 02:37:55 +0000

    build: fix CUDA linkage and add helpers

--- src/ds4_cuda.cu
+++ src/ds4_cuda.cu
@@ -1,7 +1,10 @@
 #include "ds4/cuda.h"
+#include "ds4/log.h"
 
 #if defined(DS4_HAS_CUDA)
 #include <cuda_runtime.h>
+
+extern "C" {
 
 ds4_cuda_status_t ds4_cuda_ok(void)
 {
@@ -42,6 +45,42 @@ ds4_cuda_status_t ds4_cuda_last_error(void)
 	if ( err == cudaSuccess )
 		return(ds4_cuda_ok());
 	return(ds4_cuda_fail((int32_t)err));
+}
+
+ds4_cuda_status_t ds4_cuda_peek_last_error(void)
+{
+	cudaError_t err;
+	err = cudaPeekAtLastError();
+	if ( err == cudaSuccess )
+		return(ds4_cuda_ok());
+	return(ds4_cuda_fail((int32_t)err));
+}
+
+ds4_cuda_status_t ds4_cuda_device_synchronize(void)
+{
+	cudaError_t err;
+	err = cudaDeviceSynchronize();
+	if ( err == cudaSuccess )
+		return(ds4_cuda_ok());
+	return(ds4_cuda_fail((int32_t)err));
+}
+
+ds4_cuda_status_t ds4_cuda_check_i32(int32_t cuda_err,const char *expr,const char *file,int32_t line)
+{
+	cudaError_t err;
+	if ( cuda_err == 0 )
+		return(ds4_cuda_ok());
+	if ( cuda_err < 0 )
+		return(ds4_cuda_fail(cuda_err));
+	err = (cudaError_t)cuda_err;
+	if ( expr == 0 )
+		expr = "?";
+	if ( file == 0 )
+		file = "?";
+	DS4_LOGE("cuda: %s:%d %s failed: %s",file,line,expr,cudaGetErrorString(err));
+	return(ds4_cuda_fail(cuda_err));
+}
+
 }
 #else
 #error "ds4_cuda.cu must only compile when DS4_HAS_CUDA is set"
--- src/ds4_cuda_stub.c
+++ src/ds4_cuda_stub.c
@@ -35,3 +35,23 @@ ds4_cuda_status_t ds4_cuda_last_error(void)
 {
 	return(ds4_cuda_fail(DS4_CUDA_ERR_DISABLED));
 }
+
+ds4_cuda_status_t ds4_cuda_peek_last_error(void)
+{
+	return(ds4_cuda_fail(DS4_CUDA_ERR_DISABLED));
+}
+
+ds4_cuda_status_t ds4_cuda_device_synchronize(void)
+{
+	return(ds4_cuda_fail(DS4_CUDA_ERR_DISABLED));
+}
+
+ds4_cuda_status_t ds4_cuda_check_i32(int32_t cuda_err,const char *expr,const char *file,int32_t line)
+{
+	DS4_UNUSED(expr);
+	DS4_UNUSED(file);
+	DS4_UNUSED(line);
+	if ( cuda_err == 0 )
+		return(ds4_cuda_ok());
+	return(ds4_cuda_fail(cuda_err));
+}
