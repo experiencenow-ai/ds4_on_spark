b86c4819de98dc45e84e7c70387010d0c7c3ca8c
ethpred <144991674+ethpred@users.noreply.github.com>
2026-05-10 22:59:26 +0000

    build-skeleton: keep DS4_HAS_CUDA #ifdef semantics

--- cmake/ds4_cuda.cmake
+++ cmake/ds4_cuda.cmake
@@ -3,6 +3,7 @@ function(ds4_target_cuda target)
 	target_sources(${target} PRIVATE
 		src/ds4_cuda.cu
 	)
+	target_compile_definitions(${target} PUBLIC DS4_HAS_CUDA=1)
 	set_source_files_properties(src/ds4_cuda.cu PROPERTIES LANGUAGE CUDA)
 	set_target_properties(${target} PROPERTIES CUDA_SEPARABLE_COMPILATION OFF)
 
