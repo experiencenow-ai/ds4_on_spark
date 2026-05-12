7a521f30a45f7640c16b0aca01994b1fa48b011c
ethpred <144991674+ethpred@users.noreply.github.com>
2026-05-12 04:29:05 +0000

    build-skeleton: format log ring entries

--- src/ds4_log_ring.c
+++ src/ds4_log_ring.c
@@ -1,6 +1,9 @@
 #include "ds4/log_ring.h"
 #include "ds4/str.h"
 
+#include <stddef.h>
+#include <stdio.h>
+
 static void ds4_log_ring_entry_from_msg(ds4_log_entry_t *e,int32_t level,const char *msg)
 {
 	int32_t i,n,copy;
@@ -20,6 +23,38 @@ static void ds4_log_ring_entry_from_msg(ds4_log_entry_t *e,int32_t level,const c
 	for (i=0; i<copy; i++)
 		e->msg[i] = msg[i];
 	e->msg[copy] = 0;
+}
+
+int32_t ds4_log_entry_format(const ds4_log_entry_t *e,char *out,int32_t cap)
+{
+	const char *lvl,*msg,*suffix;
+	int32_t n;
+	if ( out == 0 )
+		return(-1);
+	if ( cap <= 0 )
+		return(-2);
+	out[0] = 0;
+	if ( e == 0 )
+		return(-3);
+	lvl = ds4_log_level_name(e->level);
+	if ( lvl == 0 )
+		lvl = "?";
+	msg = e->msg;
+	if ( msg == 0 )
+		msg = "";
+	suffix = "";
+	if ( e->truncated != 0 )
+		suffix = " [truncated]";
+	n = (int32_t)snprintf(out,(size_t)cap,"%s: %s%s",lvl,msg,suffix);
+	if ( n < 0 )
+	{
+		out[0] = 0;
+		return(-4);
+	}
+	out[cap - 1] = 0;
+	if ( n >= cap )
+		return(-5);
+	return(n);
 }
 
 int32_t ds4_log_ring_init(ds4_log_ring_t *lr,ds4_log_entry_t *entries,int32_t entry_count)
@@ -69,4 +104,3 @@ void ds4_log_ring_sink(void *ctx,int32_t level,const char *msg)
 		return;
 	ds4_ring_push(&lr->r,&e);
 }
-
