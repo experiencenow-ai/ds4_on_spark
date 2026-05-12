84265d32b32cc681063d5c36ef02c44a44b70efb
ethpred <144991674+ethpred@users.noreply.github.com>
2026-05-12 03:58:46 +0000

    build-skeleton: add public header smoke compile

---
+++ tests/header_smoke_cuda.c
@@ -0,0 +1,9 @@
+#include "ds4/cuda.h"
+
+int32_t ds4_header_smoke_cuda(void);
+
+int32_t ds4_header_smoke_cuda(void)
+{
+	return(0);
+}
+
---
+++ tests/header_smoke_ring.c
@@ -0,0 +1,9 @@
+#include "ds4/ring.h"
+
+int32_t ds4_header_smoke_ring(void);
+
+int32_t ds4_header_smoke_ring(void)
+{
+	return(0);
+}
+
