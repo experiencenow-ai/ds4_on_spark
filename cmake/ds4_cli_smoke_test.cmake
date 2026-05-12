58904a97c3e882f4ee2ecab8585aa3fa3e236054
ethpred <144991674+ethpred@users.noreply.github.com>
2026-05-12 07:00:29 +0000

    build-skeleton: dump config key help

--- cmake/ds4_cli_smoke_test.cmake
+++ cmake/ds4_cli_smoke_test.cmake
@@ -61,6 +61,27 @@ if(DS4_MODE STREQUAL "dump_config_keys")
 	return()
 endif()
 
+if(DS4_MODE STREQUAL "dump_config_help")
+	execute_process(
+		COMMAND "${DS4_CLI_PATH}" --dump-config-help
+		OUTPUT_VARIABLE _ds4_out
+		ERROR_VARIABLE _ds4_err
+		RESULT_VARIABLE _ds4_rv
+	)
+	if(NOT _ds4_rv EQUAL 0)
+		message(FATAL_ERROR "ds4_cli failed: rv=${_ds4_rv}\nstderr:\n${_ds4_err}\nstdout:\n${_ds4_out}")
+	endif()
+	string(FIND "${_ds4_out}" "log_level:" _ds4_idx1)
+	if(_ds4_idx1 EQUAL -1)
+		message(FATAL_ERROR "ds4_cli output missing 'log_level:'\nstdout:\n${_ds4_out}")
+	endif()
+	string(FIND "${_ds4_out}" "enable_cuda:" _ds4_idx2)
+	if(_ds4_idx2 EQUAL -1)
+		message(FATAL_ERROR "ds4_cli output missing 'enable_cuda:'\nstdout:\n${_ds4_out}")
+	endif()
+	return()
+endif()
+
 if(DS4_MODE STREQUAL "ctx_smoke")
 	execute_process(
 		COMMAND "${DS4_CLI_PATH}" --no-cuda --arena-size 16384 --log-ring-entries 4 --smoke-ctx
